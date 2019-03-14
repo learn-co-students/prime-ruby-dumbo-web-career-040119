6421881e891fb535e25d976bf5335c67234285ed
Adam Kearney <aklives@hotmail.com>
2019-03-14 02:21:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,20 +1,9 @@
-def prime? num
-    if num <= 1
-      is_prime = false
-      return is_prime
-    end
-
-    if num == 2
-      is_prime = true
-      return is_prime
-    end
-
-    for i in 2..num
-      is_prime = false
-      if num != i && num % i == 0
-        is_prime = true
-      end
-    end
-
-return is_prime
+def prime?(num)
+  if num < 2
+    return false
+  end
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
 end
