02e4bdf6285e7628b9c16f741bcbf06e90fcd54d
Adam Kearney <aklives@hotmail.com>
2019-03-14 02:16:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime? num
+    if num <= 1
+      is_prime = false
+      return is_prime
+    end
+
+    if num == 2
+      is_prime = true
+      return is_prime
+    end
+
+    for i in 2..num
+      is_prime = false
+      if num != i && num % i == 0
+        is_prime = true
+      end
+    end
+
+return is_prime
+end
